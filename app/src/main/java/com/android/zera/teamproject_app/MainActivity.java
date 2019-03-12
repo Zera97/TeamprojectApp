1a30a052e0aa079f8e22ca068d8f6ac7aeead98c
Fabian <Fabian.theuerkauf@web.de>
2019-03-12 14:17:43 +0100

    Karte.

--- app/src/main/java/com/android/zera/teamproject_app/MainActivity.java
+++ app/src/main/java/com/android/zera/teamproject_app/MainActivity.java
@@ -1,13 +1,73 @@
 package com.android.zera.teamproject_app;
 
+import android.Manifest;
+import android.content.Context;
+import android.preference.PreferenceManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MapView map = null;
+    private IMapController mapController = null;
+    private MyLocationNewOverlay mLocationOverlay = null;
+    private Context ctx;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ctx = getApplicationContext();
+
+        ActivityCompat.requestPermissions(MainActivity.this, new String[] {
+                Manifest.permission.ACCESS_FINE_LOCATION},1);
+
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+
+
+        map = (MapView) findViewById(R.id.map);
+        map.setMultiTouchControls(true);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+
+
+        mapController =  map.getController();
+        mapController.setZoom(18.0);
+        GeoPoint startPoint = new GeoPoint(51.826918, 10.760942);
+        mapController.setCenter(startPoint);
+
+        this.mLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(ctx),map);
+        this.mLocationOverlay.enableMyLocation();
+        this.mLocationOverlay.enableFollowLocation();
+        map.getOverlays().add(this.mLocationOverlay);
+
+    }
+
+
+    public void onResume(){
+        super.onResume();
+        //this will refresh the osmdroid configuration on resuming.
+        //if you make changes to the configuration, use
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+        map.onResume(); //needed for compass, my location overlays, v6.0.0 and up
+    }
+
+    public void onPause(){
+        super.onPause();
+        //this will refresh the osmdroid configuration on resuming.
+        //if you make changes to the configuration, use
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //Configuration.getInstance().save(this, prefs);
+        map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
     }
 }
