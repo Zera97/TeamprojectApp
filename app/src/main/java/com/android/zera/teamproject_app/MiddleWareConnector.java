a556e441581348f65d25de26e1a2283f00403695
Dirk <dirkneumann98@gmail.com>
2019-03-15 11:05:46 +0100

    Einbau Socket

--- app/src/main/java/com/android/zera/teamproject_app/MiddleWareConnector.java
+++ app/src/main/java/com/android/zera/teamproject_app/MiddleWareConnector.java
@@ -2,10 +2,16 @@ package com.android.zera.teamproject_app;
 
 import android.app.Activity;
 import android.os.AsyncTask;
+import android.util.Log;
 import android.widget.Button;
-import java.lang.ref.WeakReference;
 
-public class MiddleWareConnector extends AsyncTask<String,Integer,String> {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.lang.ref.WeakReference;
+import java.net.Socket;
+
+public class MiddleWareConnector extends AsyncTask<String, Integer, String> {
 
     //Art der Parameter,Progress,Rückgabe
 
@@ -18,10 +24,37 @@ public class MiddleWareConnector extends AsyncTask<String,Integer,String> {
 
     @Override
     protected String doInBackground(String... strings) {
+        String ip = "";
+        int port = -1;
+        Socket socket = null;
+        PrintWriter out = null;
+        BufferedReader in = null;
+        String response;
+        try {
+            socket = new Socket(ip, port);
+            out = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        } catch (Exception e) {
+            return e.getMessage();
+        }
 
-        //Hier passiert die Socket Magie.
+        try {
+            out.print(new Integer(5).toString());
+            response = in.readLine();
+        } catch (Exception e) {
+            return e.getMessage();
+        }
 
-        return null;
+        try {
+            out.close();
+            in.close();
+            socket.close();
+        } catch (Exception e) {
+            Log.e("AsyncTask", "Fehler beim Schließen der Streams/des Sockets.");
+        }
+
+
+        return response;
     }
 
     @Override
@@ -32,9 +65,11 @@ public class MiddleWareConnector extends AsyncTask<String,Integer,String> {
     }
 
     @Override
-    protected void onPreExecute() {}
+    protected void onPreExecute() {
+    }
 
     @Override
-    protected void onProgressUpdate(Integer... values) {}
+    protected void onProgressUpdate(Integer... values) {
+    }
 
 }
