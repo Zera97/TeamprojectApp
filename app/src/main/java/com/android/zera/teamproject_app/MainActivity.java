ebd083dcb9049d3c6a1010113142e3a735c0c904
Fabian <Fabian.theuerkauf@web.de>
2019-03-16 19:54:51 +0100

    Ein wenig aufgeräumt

--- app/src/main/java/com/android/zera/teamproject_app/MainActivity.java
+++ app/src/main/java/com/android/zera/teamproject_app/MainActivity.java
@@ -86,82 +86,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void TestVerbindung(View v) {
-
-        boolean b = haveNetworkConnection();
+        //boolean b = haveNetworkConnection();
         //System.out.println(b ? "ja" : "nein");
+        new MiddleWareConnector(this).execute();
 
-        if(true){
-            new MiddleWareConnector(this).execute();
-        }
-        else {
-            String ip = "";
-            try {
-                Log.e("Asynctasc", "IP erzeugen");
-                InetAddress inet = Inet4Address.getByName("www.eos-noctis.de");
-                ip = inet.getHostAddress();
-            } catch (Exception e) {
-                Log.e(this.getClass().toString(), "Fehler beim Erzeugen der IP-Adresse.");
-            }
-            int port = 31896;
-
-            Socket socket = null;
-            PrintWriter out = null;
-            BufferedReader in = null;
-            String response = "";
-            try {
-                Log.e("Asynctasc", "Socket erzeugen");
-                socket = new Socket(ip, port);
-            } catch (UnknownHostException e) {
-                System.out.println("Unknown host: " + ip + " " + port);
-                //return "No Socket";
-            } catch (IOException e) {
-                System.out.println("No I/O");
-                //return "No Socket";
-            }
-            try {
-                Log.e("Asynctasc", "outStream erzeugen");
-                out = new PrintWriter(socket.getOutputStream(), true);
-            } catch (UnknownHostException e) {
-                System.out.println("Unknown host: " + ip + " " + port);
-                //return "Fehler";
-            } catch (IOException e) {
-                System.out.println("No I/O");
-                //return "Fehler";
-            }
-            try {
-                Log.e("Asynctasc", "inStream erzeugen");
-                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            } catch (UnknownHostException e) {
-                System.out.println("Unknown host: " + ip + " " + port);
-                //return "Fehler";
-            } catch (IOException e) {
-                System.out.println("No I/O");
-                //return "Fehler";
-            }
-
-            try {
-                Log.e("Asynctasc", "Hallo3");
-                out.write("Hallo");
-                out.close();
-                Log.e("Asynctasc", "Hallo4");
-                if (in.readLine() == null) {
-                    System.out.println("Server: " + "NULL");
-                }
-                Log.e("Asynctasc", "Hallo5");
-            } catch (Exception e) {
-                Log.e("AsyncTask", "Fehler beim Senden/Empfangen");
-            }
-
-            try {
-                in.close();
-                socket.close();
-            } catch (Exception e) {
-                Log.e("AsyncTask", "Fehler beim Schließen der Streams/des Sockets.");
-            }
-            Log.e("Asynctasc", "Hallo6");
-            System.out.println(response);
-            //return response;
-        }
     }
 
 
--- app/src/main/java/com/android/zera/teamproject_app/MiddleWareConnector.java
+++ app/src/main/java/com/android/zera/teamproject_app/MiddleWareConnector.java
@@ -20,12 +20,13 @@ public class MiddleWareConnector extends AsyncTask<String, Integer, String> {
 
     //Art der Parameter,Progress,Rückgabe
 
+    private final String TAG = "MiddleWareConnector";
+
     private WeakReference<Activity> parentActivity;
 
     public MiddleWareConnector(Activity activity) {
         parentActivity = new WeakReference<Activity>(activity);
     }
-
 
 
     @Override
@@ -34,80 +35,71 @@ public class MiddleWareConnector extends AsyncTask<String, Integer, String> {
         InetAddress inet = null;
         InetAddress my = null;
         String ip = "";
-        try{
-            Log.e("Asynctasc", "IP erzeugen");
+        try {
+            Log.e(TAG, "IP erzeugen");
             inet = Inet4Address.getByName("www.eos-noctis.de");
             my = InetAddress.getLocalHost();
             ip = inet.getHostAddress();
             System.out.println("ip: " + ip);
         } catch (Exception e) {
-            Log.e(this.getClass().toString(),"Fehler beim Erzeugen der IP-Adresse.");
+            Log.e(TAG, "Fehler beim Erzeugen der IP-Adresse.");
         }
-        int port = 8080;
-        ip = "192.168.0.12"; // nur in Fabis WLAN
-        ip = "46.167.0.87"; // von nirgends
+        int port = 31986;
+        //ip = "192.168.0.12"; // nur in Fabis WLAN
+        //ip = "46.167.0.87"; // von nirgends
 
         Socket socket = null;
         PrintWriter out = null;
         BufferedReader in = null;
         String response = "";
         try {
-            Log.e("Asynctasc", "Socket erzeugen");
+            Log.e(TAG, "Socket erzeugen");
             //socket = new Socket(inet, port);//, my,2000);
             socket = new Socket();
             System.out.println("izz da");
-            socket.connect(new InetSocketAddress(ip,port),20000);
+            socket.connect(new InetSocketAddress(ip, port), 20000);
             System.out.println("izz da 2");
-        }catch(UnknownHostException e) {
-            System.out.println("Unknown host: " + ip + " " + port);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
             return "No Socket";
-        } catch(IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
             return "No Socket";
         }
         try {
-            Log.e("Asynctasc", "outStream erzeugen");
-            out = new PrintWriter(socket.getOutputStream(),true);
-        }catch(UnknownHostException e) {
-            System.out.println("Unknown host: " + ip + " " + port);
-            return "Fehler";
-        } catch(IOException e) {
-            System.out.println("No I/O");
+            Log.e(TAG, "outStream erzeugen");
+            out = new PrintWriter(socket.getOutputStream(), true);
+        } catch (IOException e) {
+            e.printStackTrace();
             return "Fehler";
         }
         try {
-            Log.e("Asynctasc", "inStream erzeugen");
+            Log.e(TAG, "inStream erzeugen");
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        }catch(UnknownHostException e) {
-            System.out.println("Unknown host: " + ip + " " + port);
-            return "Fehler";
-        } catch(IOException e) {
-            System.out.println("No I/O");
+        } catch (IOException e) {
+            e.printStackTrace();
             return "Fehler";
         }
 
         try {
-            Log.e("Asynctasc","Hallo3");
+            Log.e(TAG, "Hallo3");
             byte b = 25;
             out.write(b);
             out.flush();
-            Log.e("Asynctasc","Hallo4");
+            Log.e(TAG, "Hallo4");
             System.out.println("Erg: " + in.readLine());
-            if(in.readLine() == null){
-                System.out.println("Server: " + "NULL");
-            }
-            Log.e("Asynctasc","Hallo5");
+            Log.e(TAG, "Hallo5");
         } catch (Exception e) {
-            Log.e("AsyncTask", "Fehler beim Senden/Empfangen");
+            Log.e(TAG, "Fehler beim Senden/Empfangen");
         }
 
         try {
             in.close();
             socket.close();
         } catch (Exception e) {
-            Log.e("AsyncTask", "Fehler beim Schließen der Streams/des Sockets.");
+            Log.e(TAG, "Fehler beim Schließen der Streams/des Sockets.");
         }
-        Log.e("Asynctasc","Hallo6");
+        Log.e(TAG, "Hallo6");
         System.out.println(response);
         return response;
     }
