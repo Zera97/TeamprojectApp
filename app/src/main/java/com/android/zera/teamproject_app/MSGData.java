b95d60a9b4cea296414a732feae3d95716e80229
Fabian <Fabian.theuerkauf@web.de>
2019-03-19 08:05:16 +0100

    Balken eingefügt.

--- app/src/main/java/com/android/zera/teamproject_app/MSGData.java
+++ app/src/main/java/com/android/zera/teamproject_app/MSGData.java
@@ -9,4 +9,27 @@ public class MSGData {
         this.name = name;
         this.zahl = zahl;
     }
+
+
+    /*
+     <Button
+        android:id="@+id/btn2"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:layout_toEndOf="@id/btn_verbindung"
+        android:text="HALLO"/>
+
+    <Button
+        android:id="@+id/btn3"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:text="TSCHAU"
+        android:layout_toEndOf="@id/btn2"/>
+
+    <Button
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:text="TSCHAU2"
+        android:layout_toEndOf="@id/btn3"/>
+     */
 }
