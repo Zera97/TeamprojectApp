83b0a17c6599086b4d5b8652f4e96e49a66f3488
Fabian <Fabian.theuerkauf@web.de>
2019-03-21 16:30:47 +0100

    Bissl Kram.

--- app/src/main/java/com/android/zera/teamproject_app/MainActivity.java
+++ app/src/main/java/com/android/zera/teamproject_app/MainActivity.java
@@ -2,19 +2,25 @@ package com.android.zera.teamproject_app;
 
 import android.Manifest;
 import android.content.Context;
-import android.databinding.DataBindingUtil;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Spinner;
 
-import com.android.zera.teamproject_app.databinding.ActivityMainBinding;
 import com.google.gson.Gson;
 
 import org.osmdroid.api.IMapController;
@@ -25,18 +31,13 @@ import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
+
 import java.util.ArrayList;
 
-//import com.android.zera.teamproject_app.databinding.ActivityMainBinding;
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener{
 
 
-public class MainActivity extends AppCompatActivity {
-
-    Spinner spinner;
-    FragmentOne f1;
-    FragmentTwo f2;
-    //data binding
-    private ActivityMainBinding activityMainBinding;
     private final String TAG = this.getClass().getSimpleName();
     private MapView map = null;
     private IMapController mapController = null;
@@ -50,7 +51,24 @@ public class MainActivity extends AppCompatActivity {
         // https://github.com/osmdroid/osmdroid/wiki/Markers,-Lines-and-Polygons
 
         super.onCreate(savedInstanceState);
-        activityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+       setContentView(R.layout.activity_main);
+
+        //--------------------------------Sidebar---------------------------------------------------
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //-------------------------------Sidebar-Ende-----------------------------------------------
 
         final String[] select_qualification = {
                 "Schließen", "201", "202", "203",
@@ -72,18 +90,6 @@ public class MainActivity extends AppCompatActivity {
 
 
         spinner.setAdapter(myAdapter);
-        BusRouteList b = new BusRouteList();
-        b.routes.add(new BusRouteSpinnerViewModel(201,true));
-        b.routes.add(new BusRouteSpinnerViewModel(202,false));
-        b.routes.add(new BusRouteSpinnerViewModel(203,false));
-        b.routes.add(new BusRouteSpinnerViewModel(204,true));
-        b.routes.add(new BusRouteSpinnerViewModel(205,false));
-
-        activityMainBinding.setBusrouteList(b);
-
-        BusRouteSpinnerViewModel bus = new BusRouteSpinnerViewModel(201,true);
-        activityMainBinding.setBusroute(bus);
-
 
         ctx = getApplicationContext();
 
@@ -176,4 +182,61 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
         map.onPause();
     }
+
+    //--------------------------------Sidebar-Methoden----------------------------------------------
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.impressum, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.punkt1) {
+
+        } else if (id == R.id.punkt2) {
+
+        } else if (id == R.id.punkt3) {
+
+        } else if(id == R.id.nav_contact){
+
+        }else if(id == R.id.nav_impressum){
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    //--------------------------------Sidebar-Methoden-Ende-----------------------------------------
 }
