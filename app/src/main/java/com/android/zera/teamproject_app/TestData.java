d7b848f4fe2346138675226cdcebc3f8012e587e
Fabian <Fabian.theuerkauf@web.de>
2019-04-01 19:22:33 +0200

    TestData geaddet.

---
+++ app/src/main/java/com/android/zera/teamproject_app/TestData.java
@@ -0,0 +1,30 @@
+package com.android.zera.teamproject_app;
+
+public class TestData {
+
+    String name;
+    int id;
+
+    public TestData(String name, int id){
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+}
